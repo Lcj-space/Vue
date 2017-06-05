f03f275052ab9ebec40aa728682f12a9cc46560c
Lcj-space <1367546524@qq.com>
2017-06-05 20:13:45 +0800

    将所有的碎片组件封装成一个大的组件

---
+++ vue-router/src/components/home.vue
@@ -0,0 +1,29 @@
+
+    <template>
+        <div id="home">
+            <MyInfo></MyInfo>
+            <Work></Work>
+            <Skill></Skill>
+            <MyMap></MyMap>
+            <MyFooter></MyFooter>
+        </div>
+    </template>
+    <script>
+
+        import MyInfo from  './BasicInformation.vue'
+        import Skill from  './Skill.vue'
+        import MyFooter from  './footer.vue'
+        import MyMap from  './map.vue'
+        import Work from  './works.vue'
+        export default {
+            name: 'home',
+            components: {
+                MyInfo,
+                Skill,
+                MyFooter,
+                MyMap,
+                Work
+            }
+
+        }
+    </script>
