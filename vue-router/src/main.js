0917e574845a728487f278bc27db7f6ea90271cd
Lcj-space <1367546524@qq.com>
2017-06-06 16:13:21 +0800

    将地址栏后面的#号去掉

--- vue-router/src/main.js
+++ vue-router/src/main.js
@@ -3,12 +3,15 @@ import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
-import Router from './router.config.js'
+import routes from './router.config.js'
 Vue.use(ElementUI);
 Vue.use(VueRouter);
-const router = new VueRouter(Router);
+const router = new VueRouter({
+    mode: 'history',
+    routes
+});
 new Vue({
     router,
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
--- vue-router/src/router.config.js
+++ vue-router/src/router.config.js
@@ -1,9 +1,8 @@
 import Home from './components/home.vue'
 import Ifooter from  './components/footer.vue'
-export default {
-     routes:[
-         {path:'/home',component:Home},
-         {path:'/foot',component:Ifooter},
-         {path:'*', redirect:'/home'}
-     ]
-}
+export default[
+    {path: '/home', component: Home},
+    {path: '/foot', component: Ifooter},
+    {path: '*', redirect: '/home'}
+]
+
