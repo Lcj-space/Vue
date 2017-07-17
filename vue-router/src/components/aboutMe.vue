2fcfe543417c9f00e7da299e601c887ac5efe159
chongjie.luo <chongjie.luo@qunar.com>
2017-07-17 15:52:47 +0800

    语义化修改

--- vue-router/src/components/aboutMe.vue
+++ vue-router/src/components/aboutMe.vue
@@ -2,156 +2,164 @@
     <div class="myInfo">
         <img src="src/assets/myImg.jpg"/>
         <div class="base-info">
-            <span style="font-size: 16px">基本信息：</span>
-            <div class="line">
+            <header>
+                <span style="font-size: 16px">基本信息：</span>
+                <div class="line">
 
+                </div>
+                <span>姓名：&nbsp;罗崇杰</span>
+                <span>性别：&nbsp;男</span>
+                <span>最高学历：&nbsp;本科</span>
+                <span>工作年限：&nbsp;1年</span>
+                <span>毕业时间：&nbsp;2018年</span>
+                <span>邮箱：&nbsp;1367546524@qq.com</span>
+            </header>
+            <div class="prize">
+                <span style="font-size: 16px">获奖荣誉：</span>
+                <div class="line">
+
+                </div>
+                <span class="t-title"> 2015年</span>
+                <p>
+                    获校级二级奖学金、优秀贫困大学生三等奖学金
+                </p>
+
+                <span class="t-title"> 2016年</span>
+                <p>
+                    获CET-4、黑龙江大学物理知识竞赛一等奖、获校级三等奖学金、汽车驾驶证
+
+                </p>
+                <span class="t-title"> 2017</span>
+                <p>
+                    获黑龙江大学移动开发三等奖
+                </p>
             </div>
-            <span>姓名：&nbsp;罗崇杰</span>
-            <span>性别：&nbsp;男</span>
-            <span>最高学历：&nbsp;本科</span>
-            <span>工作年限：&nbsp;1年</span>
-            <span>毕业时间：&nbsp;2018年</span>
-            <span>邮箱：&nbsp;1367546524@qq.com</span>
-            <div class="jianGe"></div>
-            <span style="font-size: 16px">获奖荣誉：</span>
-            <div class="line">
+            <div class="project-experience">
+                <span style="font-size: 16px">项目经历：</span>
+                <div class="line">
 
-            </div>
-            <span class="t-title"> 2015年</span>
-            <p>
-                获校级二级奖学金、优秀贫困大学生三等奖学金
-            </p>
-
-            <span class="t-title"> 2016年</span>
-            <p>
-                获CET-4、黑龙江大学物理知识竞赛一等奖、获校级三等奖学金、汽车驾驶证
-
-            </p>
-            <span class="t-title"> 2017</span>
-            <p>
-                获黑龙江大学移动开发三等奖
-             </p>
-            <div class="jianGe"></div>
-            <a style="font-size: 16px">项目经历：</a>
-            <div class="line">
-
-            </div>
-            <span class="t-title"> 2017.05-至今</span><br>
-            <p>
-                项目名称: &nbsp; 时光漫步
-                <span class="row-blank">
+                </div>
+                <span class="t-title"> 2017.05-至今</span><br>
+                <p>
+                    项目名称: &nbsp; 时光漫步
+                    <span class="row-blank">
 
                 </span>
-                项目职责：&nbsp; 前端负责人
-            </p>
+                    项目职责：&nbsp; 前端负责人
+                </p>
 
-            <p>
-                项目内容:<br>
-                <span>
+                <p>
+                    项目内容:<br>
+                    <span>
                     基于 vue-cli 脚手架 利用 vue2.0+ vue-router + vuex + webpack + es6 编写项目
                 </span><br>
-                <span>
+                    <span>
                     完成时光漫步的收发信件的功能，和连通数据库的交互
                   </span><br>
-                <span>使用了GIt进行代码的托管，尽可能的便面协作开发代码冲突</span><br>
-                <span>前后端并行开发的方式，控制开发的效率。遇到问题及时沟通</span><br>
-                <span>使用Vue+webpack构建单页面应用，构建时光漫步项目</span><br>
-                <span>项目即将参加全国创新创业大赛</span>
-            </p>
+                    <span>使用了GIt进行代码的托管，尽可能的便面协作开发代码冲突</span><br>
+                    <span>前后端并行开发的方式，控制开发的效率。遇到问题及时沟通</span><br>
+                    <span>使用Vue+webpack构建单页面应用，构建时光漫步项目</span><br>
+                    <span>项目即将参加全国创新创业大赛</span>
+                </p>
 
-            <span class="t-title"> 2017.02-2017.5</span><br>
-            <p>
-                项目名称: &nbsp; 悦居短租
-                <span class="row-blank">
+                <span class="t-title"> 2017.02-2017.5</span><br>
+                <p>
+                    项目名称: &nbsp; 悦居短租
+                    <span class="row-blank">
 
                 </span>
-                项目职责：&nbsp; 前端+后端（组员）
-            </p>
+                    项目职责：&nbsp; 前端+后端（组员）
+                </p>
 
-            <p>
-                项目内容:<br>
-                <span>
+                <p>
+                    项目内容:<br>
+                    <span>
                     基于CI框架 + vueJs + Bootstrap + zeptoJs + axios + mint-ui 构建移动端页面<br>
                 </span><br>
-                <span>
+                    <span>
                     网站设计是一个短租平台
                 </span><br>
-                <span>
+                    <span>
                     负责评论页面、订单页面、支付页面的编写。利用axios进行前后端的数据交互。
                 </span><br>
-                <a>
-                    后台页面利用了bootstrap，在图片的上传功能为防止恶意注入进行了后端校验
-                </a><br>
-                <span>
+                    <a>
+                        后台页面利用了bootstrap，在图片的上传功能为防止恶意注入进行了后端校验
+                    </a><br>
+                    <span>
                     页面的编写大部分是利用了vueJS提高了页面的性能。
                  </span><br>
-                网站地址:<a href="http://www.hrbyueju.com/yuejum/">http://www.hrbyueju.com/yuejum/</a><br>
+                    网站地址:<a href="http://www.hrbyueju.com/yuejum/">http://www.hrbyueju.com/yuejum/</a><br>
 
-            </p>
-            <div class="jianGe"></div>
-
-            <span style="font-size: 16px">自我描述：</span>
-            <div class="line"></div>
-            <p style="padding-left: 30px; ">
-                自我学习能力强，做事认真。编程爱好者,喜欢研究新知识。热爱折腾拥有梦想。对于工作积极负责。坚信“态度决定一切，细节决定成败”。善于沟通，善于捕捉业务逻辑。我年轻有活力有梦想，以成为一名极客而努力，我这个人运气比较好希望能把好运带给身边的人。创新能力很强，拥有较强的逻辑。最近在看平凡的世界，希望我们能够像著作中一样一起平凡而伟大。
-            </p>
+                </p>
+            </div>
+            <footer>
+                <span style="font-size: 16px">自我描述：</span>
+                <div class="line"></div>
+                <p style="padding-left: 30px; ">
+                    自我学习能力强，做事认真。编程爱好者,喜欢研究新知识。热爱折腾拥有梦想。对于工作积极负责。坚信“态度决定一切，细节决定成败”。善于沟通，善于捕捉业务逻辑。我年轻有活力有梦想，以成为一名极客而努力，我这个人运气比较好希望能把好运带给身边的人。创新能力很强，拥有较强的逻辑。最近在看平凡的世界，希望我们能够像著作中一样一起平凡而伟大。
+                </p>
+            </footer>
         </div>
     </div>
 </template>
 
 
-
-<style scoped>
+<style lang="scss" scoped>
     .myInfo {
         width: 1080px;
         margin: 0 auto;
         padding: 60px 0 60px 0;
         color: #5e6d82;
+
+        .line {
+            margin-bottom: 10px;
+            border: 1px solid;
+        }
+
+        img {
+            display: block;
+            position: relative;
+            border-radius: 50%;
+            width: 130px;
+            height: 130px;
+            margin: 10px auto;
+            filter: Alpha(opacity=0.6); /* IE */
+            -moz-opacity: 0.6; /* FF 主要是为了兼容老版本的FF */
+            opacity: 0.6; /* FF */
+        }
+
+        .base-info {
+            margin: 20px 195px 0 195px;
+            width: 730px;
+            span {
+                display: inline-block;
+                width: 240px;
+                line-height: 40px;
+            }
+            .prize, .project-experience {
+                padding-bottom: 20px;
+            }
+            p {
+                a {
+                    display: inline-block;
+                    text-indent: 40px;
+                }
+                span {
+                    width: 100%;
+                    height: 21px;
+                    line-height: 21px;
+                    text-indent: 40px;
+                }
+            }
+            .t-title {
+                line-height: 30px;
+            }
+            .row-blank {
+                display: inline-block;
+                width: 40px;
+            }
+        }
+
     }
 
-    .myInfo .line {
-        margin-bottom: 10px;
-        border: 1px solid;
-    }
-
-    .myInfo img {
-        display: block;
-        position: relative;
-        border-radius: 50%;
-        width: 130px;
-        height: 130px;
-        margin: 10px auto;
-        filter: Alpha(opacity=0.6); /* IE */
-        -moz-opacity: 0.6; /* FF 主要是为了兼容老版本的FF */
-        opacity: 0.6; /* FF */
-    }
-
-    .base-info {
-        margin: 20px 195px 0 195px;
-        width: 730px;
-    }
-
-    .base-info span {
-        display: inline-block;
-        width: 240px;
-        line-height: 40px;
-    }
-
-    .base-info .jianGe {
-        height: 20px;
-    }
-
-    .base-info p a {
-        display: inline-block;
-        text-indent: 40px;
-    }
-
-    .base-info .t-title {
-        line-height: 30px;
-    }
-
-    .base-info .row-blank {
-        display: inline-block;
-        width: 40px;
-    }
 </style>
